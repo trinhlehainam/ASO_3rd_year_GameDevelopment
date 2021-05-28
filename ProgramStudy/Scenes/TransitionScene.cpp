47191b8f841555f0ba9cfcf8702875ca578bd1df
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2021-05-28 11:17:02 +0900

    Implement Transition Stages
    - Add Cross Over stage

--- ProgramStudy/Scenes/TransitionScene.cpp
+++ ProgramStudy/Scenes/TransitionScene.cpp
@@ -2,9 +2,35 @@
 
 #include <DxLib.h>
 
+namespace
+{
+    constexpr float kTransitionTime = 3.0f;
+}
+
 TransitionScene::TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after):
     m_before(std::move(before)), m_after(std::move(after)), 
-    m_timer_s(1.0f),
+    m_stageBefore(STAGE::CROSS_OVER), m_stageAfter(STAGE::CROSS_OVER),
+    m_timer_s(kTransitionTime),
+    m_updateFunc(&TransitionScene::UpdateCrossOverScene),
+    m_renderFunc(&TransitionScene::RenderCrossOverScene)
+{
+    m_after->Init();
+}
+
+TransitionScene::TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after, STAGE stage):
+    m_before(std::move(before)), m_after(std::move(after)),
+    m_stageBefore(stage), m_stageAfter(stage),
+    m_timer_s(kTransitionTime),
+    m_updateFunc(&TransitionScene::UpdateBeforeScene),
+    m_renderFunc(&TransitionScene::RenderBeforeScene)
+{
+     m_after->Init();
+}
+
+TransitionScene::TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after, STAGE stageBefore, STAGE stageAfter):
+    m_before(std::move(before)), m_after(std::move(after)),
+    m_stageBefore(stageBefore), m_stageAfter(stageAfter),
+    m_timer_s(kTransitionTime),
     m_updateFunc(&TransitionScene::UpdateBeforeScene),
     m_renderFunc(&TransitionScene::RenderBeforeScene)
 {
@@ -20,9 +46,10 @@ bool TransitionScene::Init()
     return true;
 }
 
-void TransitionScene::Update(float deltaTime_ms)
+void TransitionScene::Update(float deltaTime_s)
 {
-    (this->*m_updateFunc)(deltaTime_ms);
+    (this->*m_updateFunc)(deltaTime_s);
+    m_timer_s -= deltaTime_s;
 }
 
 void TransitionScene::Render()
@@ -36,36 +63,56 @@ std::unique_ptr<IScene> TransitionScene::ChangeScene(std::unique_ptr<IScene> sce
     return std::move(m_after);
 }
 
-void TransitionScene::UpdateBeforeScene(float deltaTime_ms)
+void TransitionScene::UpdateBeforeScene(float deltaTime_s)
 {
     if (m_timer_s < 0.0f)
     {
-        m_timer_s = 3.0f;
+        m_timer_s = kTransitionTime;
         m_updateFunc = &TransitionScene::UpdateAfterScene;
         m_renderFunc = &TransitionScene::RenderAfterScene;
     }
-    m_timer_s -= deltaTime_ms;
 }
 
-void TransitionScene::UpdateAfterScene(float deltaTime_ms)
+void TransitionScene::UpdateAfterScene(float deltaTime_s)
 {
     if (m_timer_s < 0.0f)
     {
-        m_timer_s = 255;
+        m_timer_s = kTransitionTime;
+        EnableChangeScene = true;
+    }
+}
+
+void TransitionScene::UpdateCrossOverScene(float deltaTime_s)
+{
+    if (m_timer_s < 0.0f)
+    {
+        m_timer_s = kTransitionTime;
         EnableChangeScene = true;
     }
 }
 
 void TransitionScene::RenderBeforeScene()
 {
-    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, m_timer_s);
+    int alpha = static_cast<int>((m_timer_s * 255) / kTransitionTime);
+    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, alpha);
     m_before->Render();
     DxLib::SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 255);
 }
 
 void TransitionScene::RenderAfterScene()
 {
-    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, m_timer_s);
+    int alpha = static_cast<int>((m_timer_s * 255) / kTransitionTime);
+    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, 255 - alpha);
+    m_after->Render();
+    DxLib::SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 255);
+}
+
+void TransitionScene::RenderCrossOverScene()
+{
+    int alpha = static_cast<int>((m_timer_s * 255) / kTransitionTime);
+    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, alpha);
+    m_before->Render();
+    DxLib::SetDrawBlendMode(DX_BLENDMODE_ADD, 255 - alpha);
     m_after->Render();
     DxLib::SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 255);
 }
--- ProgramStudy/Scenes/TransitionScene.h
+++ ProgramStudy/Scenes/TransitionScene.h
@@ -5,13 +5,16 @@ class TransitionScene :
     public IScene
 {
 public:
-    enum class LAYOUT
+    enum class STAGE
     {
         FADE_IN,
-        FADE_OUT
+        FADE_OUT,
+        CROSS_OVER
     };
 public:
     TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after);
+    TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after, STAGE stage);
+    TransitionScene(std::unique_ptr<IScene> before, std::unique_ptr<IScene> after, STAGE stageBefore, STAGE stageAfter );
     ~TransitionScene();
 private:
    bool Init() override;
@@ -23,13 +26,18 @@ private:
    using UpdateFunc_t = void (TransitionScene::*)(float);
    using RenderFunc_t = void (TransitionScene::*)();
    
-   void UpdateBeforeScene(float deltaTime);
-   void UpdateAfterScene(float deltaTime);
+   void UpdateBeforeScene(float deltaTime_s);
+   void UpdateAfterScene(float deltaTime_s);
+   void UpdateCrossOverScene(float deltaTime_s);
+
    void RenderBeforeScene();
    void RenderAfterScene();
+   void RenderCrossOverScene();
 private:
     std::unique_ptr<IScene> m_before;
     std::unique_ptr<IScene> m_after;
+    STAGE m_stageBefore;
+    STAGE m_stageAfter;
 
     RenderFunc_t m_renderFunc;
     UpdateFunc_t m_updateFunc;
